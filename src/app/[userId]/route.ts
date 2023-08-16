9d6f56b22f13a865214dec45ef038f02ed8c17e5
kou12345 <hi.0412.ko.2002@gmail.com>
2023-08-16 16:28:31 +0900

    add POST wip

--- src/app/[userId]/route.ts
+++ src/app/[userId]/route.ts
@@ -1,9 +1,12 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Database } from "../../../database.types";
 
-export async function GET({ params }: { params: { userId: string } }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { userId: string } }
+) {
   const supabase = createServerComponentClient<Database>({ cookies });
 
   const userId = params.userId;
@@ -22,6 +25,36 @@ export async function GET({ params }: { params: { userId: string } }) {
     if (data) {
       return NextResponse.json({ data }, { status: 200 });
     }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("error", error.message);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  }
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { userId: string } }
+) {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const userId = params.userId;
+
+  try {
+    const formData = await request.formData();
+
+    const fullname = formData.get("fullname");
+    const username = formData.get("username");
+    const website = formData.get("website");
+
+    console.log("formData: ", formData.get("hoge"));
+
+    console.log("fullname: ", fullname);
+    console.log("username: ", username);
+    console.log("website: ", website);
+
+    return NextResponse.json({ data: "ok" }, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       console.error("error", error.message);
