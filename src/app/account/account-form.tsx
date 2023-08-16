6381c263884d770d772163a966fc1ef941f0da67
kou12345 <hi.0412.ko.2002@gmail.com>
2023-08-16 10:50:45 +0900

    wip

--- src/app/account/account-form.tsx
+++ src/app/account/account-form.tsx
@@ -3,9 +3,9 @@ import {
   Session,
   createClientComponentClient,
 } from "@supabase/auth-helpers-nextjs";
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
+import useSWR from "swr";
 import { Database } from "../../../database.types";
-import Avatar from "./avatar";
 
 export default function AccountForm({ session }: { session: Session | null }) {
   const supabase = createClientComponentClient<Database>();
@@ -16,129 +16,140 @@ export default function AccountForm({ session }: { session: Session | null }) {
   const [avatar_url, setAvatarUrl] = useState<string | null>(null);
   const user = session?.user;
 
-  const getProfile = useCallback(async () => {
-    try {
-      setLoading(true);
+  console.log(user);
 
-      let { data, error, status } = await supabase
-        .from("profiles")
-        .select(`full_name, username, website, avatar_url`)
-        .eq("id", user?.id)
-        .single();
+  console.log(fullname);
 
-      if (error && status !== 406) {
-        throw error;
-      }
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-      if (data) {
-        setFullname(data.full_name);
-        setUsername(data.username);
-        setWebsite(data.website);
-        setAvatarUrl(data.avatar_url);
-      }
-    } catch (error) {
-      alert("Error loading user data!");
-    } finally {
-      setLoading(false);
-    }
-  }, [user, supabase]);
+  // app/[userName]/route.tsにRequestする
+  const { data, error, isLoading } = useSWR(`/${user?.id}/`, fetcher);
 
-  useEffect(() => {
-    getProfile();
-  }, [user, getProfile]);
+  console.log(data);
 
-  async function updateProfile({
-    username,
-    website,
-    avatar_url,
-  }: {
-    username: string | null;
-    fullname: string | null;
-    website: string | null;
-    avatar_url: string | null;
-  }) {
-    try {
-      setLoading(true);
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
 
-      let { error } = await supabase.from("profiles").upsert({
-        id: user?.id as string,
-        full_name: fullname,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date().toISOString(),
-      });
-      if (error) throw error;
-      alert("Profile updated!");
-    } catch (error) {
-      alert("Error updating the data!");
-    } finally {
-      setLoading(false);
-    }
-  }
+  // setFullname(data?.full_name);
+
+  console.log(fullname);
 
   return (
-    <div className="form-widget">
-      <Avatar
-        uid={user?.id || ""}
-        url={avatar_url}
-        size={150}
-        onUpload={(url) => {
-          setAvatarUrl(url);
-          updateProfile({ fullname, username, website, avatar_url: url });
-        }}
+    <div>
+      <label htmlFor="email">Email</label>
+      <input type="text" defaultValue={user?.email} disabled />
+
+      <label htmlFor="fullname">Full Name</label>
+      <input
+        type="text"
+        defaultValue={data.full_name || ""}
+        onChange={(e) => setFullname(e.target.value)}
       />
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session?.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="fullName">Full Name</label>
-        <input
-          id="fullName"
-          type="text"
-          value={fullname || ""}
-          onChange={(e) => setFullname(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ""}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="url"
-          value={website || ""}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
 
-      <div>
-        <button
-          className="button primary block"
-          onClick={() =>
-            updateProfile({ fullname, username, website, avatar_url })
-          }
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
-        </button>
-      </div>
+      <label htmlFor="username">User Name</label>
+      <input type="text" defaultValue={data.username} />
 
-      <div>
-        <form action="/auth/signout" method="post">
-          <button className="button block" type="submit">
-            Sign out
-          </button>
-        </form>
-      </div>
+      <label htmlFor="website">Website</label>
+      <input type="text" defaultValue={data.website} />
+
+      <label htmlFor="avatar_url">Avatar</label>
+      <input type="text" defaultValue={data.avatar_url} />
+
     </div>
   );
+
+  //   async function updateProfile({
+  //     username,
+  //     website,
+  //     avatar_url,
+  //   }: {
+  //     username: string | null;
+  //     fullname: string | null;
+  //     website: string | null;
+  //     avatar_url: string | null;
+  //   }) {
+  //     try {
+  //       setLoading(true);
+
+  //       let { error } = await supabase.from("profiles").upsert({
+  //         id: user?.id as string,
+  //         full_name: fullname,
+  //         username,
+  //         website,
+  //         avatar_url,
+  //         updated_at: new Date().toISOString(),
+  //       });
+  //       if (error) throw error;
+  //       alert("Profile updated!");
+  //     } catch (error) {
+  //       alert("Error updating the data!");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+
+  //   return (
+  //     <div className="form-widget">
+  //       <Avatar
+  //         uid={user?.id || ""}
+  //         url={avatar_url}
+  //         size={150}
+  //         onUpload={(url) => {
+  //           setAvatarUrl(url);
+  //           updateProfile({ fullname, username, website, avatar_url: url });
+  //         }}
+  //       />
+  //       <div>
+  //         <label htmlFor="email">Email</label>
+  //         <input id="email" type="text" value={session?.user.email} disabled />
+  //       </div>
+  //       <div>
+  //         <label htmlFor="fullName">Full Name</label>
+  //         <input
+  //           id="fullName"
+  //           type="text"
+  //           value={fullname || ""}
+  //           onChange={(e) => setFullname(e.target.value)}
+  //         />
+  //       </div>
+  //       <div>
+  //         <label htmlFor="username">Username</label>
+  //         <input
+  //           id="username"
+  //           type="text"
+  //           value={username || ""}
+  //           onChange={(e) => setUsername(e.target.value)}
+  //         />
+  //       </div>
+  //       <div>
+  //         <label htmlFor="website">Website</label>
+  //         <input
+  //           id="website"
+  //           type="url"
+  //           value={website || ""}
+  //           onChange={(e) => setWebsite(e.target.value)}
+  //         />
+  //       </div>
+
+  //       <div>
+  //         <button
+  //           className="button primary block"
+  //           onClick={() =>
+  //             updateProfile({ fullname, username, website, avatar_url })
+  //           }
+  //           disabled={loading}
+  //         >
+  //           {loading ? "Loading ..." : "Update"}
+  //         </button>
+  //       </div>
+
+  //       <div>
+  //         <form action="/auth/signout" method="post">
+  //           <button className="button block" type="submit">
+  //             Sign out
+  //           </button>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   );
 }
